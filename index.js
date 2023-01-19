1092ae51bbb15ec41f099fc475f5819701802994
Arwin01 <117560564+Arwin01@users.noreply.github.com>
2023-01-19 20:28:45 +0530

    Added DOM
    tried DOM and objects for the first time

---
+++ index.js
@@ -0,0 +1,74 @@
+var noofbuttons = document.querySelectorAll(".drum").length;
+for (var i = 0; i<noofbuttons; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+
+    var buttoninnerHTML = this.innerHTML;
+
+    makessound(buttoninnerHTML);
+    buttonanimation(buttoninnerHTML);
+  });
+
+}
+
+document.addEventListener("keydown",function(event){
+makessound(event.key);
+buttonanimation(event.key);
+});
+
+
+
+
+function makessound(key){
+  switch (key) {
+    case "s":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "d":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+
+    case "j":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "k":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "l":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      var kickbass = new Audio("sounds/kickbass.mp3");
+      kickbass.play();
+      break;
+
+    case "w":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    default:
+      console.log(buttoninnerHTML);
+  }
+}
+
+
+ function buttonanimation(press){
+    var activebutton= document.querySelector("."+press);
+   activebutton.classList.add("pressed")
+      setTimeout(function(){
+        activebutton.classList.remove("pressed");
+      },100);
+    }
+   
+ 
